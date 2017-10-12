5ff6449ca5ab1ed563aa2d36b4cfee290927e904
Jens Breitbart <jbreitbart@gmail.com>
2017-10-12 23:43:49 +0200

    Initial version of working opticat.

--- src/opticat.cpp
+++ src/opticat.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <vector>
 
+#include <cassert>
 #include <cstring>
 
 // for perf support
@@ -12,12 +13,43 @@
 #include <unistd.h>
 
 #include <distgen/distgen.h>
+#include <ponci/ponci.hpp>
 #include <ponri/ponri.hpp>
 
+/*** constants **/
 const std::string res_name("opticat");
+const std::chrono::seconds measurement_time(10);
 
 /*** config vars **/
 static distgend_initT distgen_init;
+static std::string command;
+
+static std::vector<std::thread> thread_pool;
+
+static void setup_cat() {
+	resgroup_create(res_name);
+	cgroup_create(res_name);
+
+	// use all selected threads for this group
+	// TODO change to number of threads per NUMA domain
+	std::vector<size_t> cpus;
+	for (size_t i = 0; i < distgen_init.number_of_threads; ++i) {
+		cpus.push_back(i);
+	}
+
+	resgroup_set_cpus(res_name, cpus);
+	cgroup_set_cpus(res_name, cpus);
+
+	size_t mems[] = {0, 1};
+	cgroup_set_mems(res_name, mems, 2);
+}
+
+[[noreturn]] static void cleanup() {
+	cgroup_kill(res_name);
+	resgroup_delete(res_name);
+	thread_pool[0].join();
+	exit(0);
+}
 
 [[noreturn]] static void print_help(const char *argv) {
 	std::cout << argv << " supports the following flags:\n";
@@ -25,13 +57,14 @@ static distgend_initT distgen_init;
 	std::cout << "\t --threads \t Number of logical cores. \t\t\t Default: " << std::thread::hardware_concurrency()
 			  << "\n";
 	std::cout << "\t --smt \t\t Number of logical cores per physical core. \t Default: 2\n";
+	std::cout << "\t -- <command> \t The command to be executed. \t No default\n";
 	exit(0);
 }
 
 static void parse_options(size_t argc, const char **argv) {
-	// if (argc == 1) {
-	//		print_help(argv[0]);
-	//}
+	if (argc == 1) {
+		print_help(argv[0]);
+	}
 
 	distgen_init.number_of_threads = std::thread::hardware_concurrency();
 	distgen_init.NUMA_domains = 2;
@@ -68,7 +101,19 @@ static void parse_options(size_t argc, const char **argv) {
 			++i;
 			continue;
 		}
+		if (arg == "--") {
+			if (i + 1 >= argc) {
+				print_help(argv[0]);
+			}
+			++i;
+			for (; i < argc; ++i) {
+				command += std::string(argv[i]) + " ";
+			}
+			return;
+		}
 	}
+
+	print_help(argv[0]);
 }
 
 static int perf_event_open(struct perf_event_attr *hw_event, pid_t pid, size_t cpu, int group_fd, unsigned long flags) {
@@ -133,8 +178,35 @@ static std::vector<long long> read_perf_measurement(const std::vector<int> &fds)
 	return counts;
 }
 
+// run distgen to evict everything from the cache
+static void clear_cache() {
+	cgroup_freeze(res_name);
+
+	distgend_configT dg;
+	dg.number_of_threads = distgen_init.number_of_threads;
+	for (unsigned char i = 0; i < distgen_init.number_of_threads; ++i) dg.threads_to_use[i] = i;
+	distgend_is_membound(dg);
+
+	cgroup_thaw(res_name);
+}
+
+static void execute_command_internal(std::string command) {
+	cgroup_add_me(res_name);
+	// command += "| tee ";
+	command += "> ";
+	command += "cmd.log";
+	command += " 2>&1 ";
+
+	auto temp = system(command.c_str());
+	assert(temp != -1);
+}
+
+static void execute_command(std::string command) { thread_pool.emplace_back(&execute_command_internal, command); }
+
 int main(int argc, char const *argv[]) {
 	parse_options(static_cast<size_t>(argc), argv);
+
+	std::cout << "Analysing " << command << std::endl << std::endl;
 
 	std::cout << "Starting distgen initialization ...";
 	std::cout.flush();
@@ -144,44 +216,36 @@ int main(int argc, char const *argv[]) {
 	std::cout << "CBM max: " << std::hex << get_cbm_mask_as_uint() << std::endl;
 	std::cout << "CBM min bits: " << get_min_cbm_bits() << std::endl;
 	std::cout << "Number of closids: " << get_num_closids() << std::endl;
+	std::cout << std::endl;
 
 	auto perf_ids = init_perf();
 
-	resgroup_create(res_name);
-	{
-		std::vector<size_t> cpus;
-		for (size_t i = 0; i < distgen_init.number_of_threads; ++i) {
-			cpus.push_back(i);
-		}
-		resgroup_set_cpus(res_name, cpus);
-	}
-
-	// loop over all possible settings
-	auto bits = get_cbm_mask();
+	setup_cat();
 
 	std::cout << "Mask"
 			  << " - "
 			  << "cache misses" << std::endl;
 
+	// loop over all possible settings
+	auto bits = get_cbm_mask();
+
+	execute_command(command);
+	// wait some time before we start measurements
+	std::this_thread::sleep_for(measurement_time);
+
 	while (bits.count() >= get_min_cbm_bits()) {
+
 		std::vector<size_t> schematas;
 		for (size_t n = 0; n < distgen_init.NUMA_domains; ++n) schematas.push_back(bits.to_ullong());
 		resgroup_set_schemata(res_name, schematas);
 
+		clear_cache();
+
 		start_perf_measurement(perf_ids);
-
-		// TODO start real application, read command from file?
-
-		// run distgen to evict everything from the cache
-		distgend_configT dg;
-		dg.number_of_threads = distgen_init.number_of_threads;
-		for (size_t i = 0; i < distgen_init.number_of_threads; ++i) dg.threads_to_use[i] = i;
-		distgend_is_membound(dg);
-
-		// TODO move stop measurement before distgen call
+		std::this_thread::sleep_for(measurement_time);
 		stop_perf_measurement(perf_ids);
-		auto res = read_perf_measurement(perf_ids);
 
+		auto res = read_perf_measurement(perf_ids);
 		double sum = 0;
 		for (size_t i = 0; i < res.size(); ++i) {
 			sum += res[i];
@@ -198,5 +262,7 @@ int main(int argc, char const *argv[]) {
 		}
 	}
 
-	resgroup_delete(res_name);
+	std::cout << "\nMeasurement done!" << std::endl;
+
+	cleanup();
 }
