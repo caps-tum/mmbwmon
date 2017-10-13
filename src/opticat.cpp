5dd15727a87d5d84ac2283f84b9c1974cd4ca2ee
Jens Breitbart <jbreitbart@gmail.com>
2017-10-13 12:14:50 +0200

    Made cache clear optional. Count instructions.

--- src/opticat.cpp
+++ src/opticat.cpp
@@ -1,4 +1,6 @@
+#include <iomanip>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <thread>
 #include <vector>
@@ -23,6 +25,7 @@ const std::chrono::seconds measurement_time(10);
 /*** config vars **/
 static distgend_initT distgen_init;
 static std::string command;
+static bool use_cache_clear = false;
 
 static std::vector<std::thread> thread_pool;
 
@@ -57,6 +60,7 @@ static void setup_cat() {
 	std::cout << "\t --threads \t Number of logical cores. \t\t\t Default: " << std::thread::hardware_concurrency()
 			  << "\n";
 	std::cout << "\t --smt \t\t Number of logical cores per physical core. \t Default: 2\n";
+	std::cout << "\t --cache-clear \t Use cache clear. \t Default: disabled\n";
 	std::cout << "\t -- <command> \t The command to be executed. \t No default\n";
 	exit(0);
 }
@@ -101,6 +105,12 @@ static void parse_options(size_t argc, const char **argv) {
 			++i;
 			continue;
 		}
+		if (arg == "--cache-clear") {
+			use_cache_clear = true;
+			++i;
+			continue;
+		}
+
 		if (arg == "--") {
 			if (i + 1 >= argc) {
 				print_help(argv[0]);
@@ -137,7 +147,18 @@ static std::vector<int> init_perf() {
 	event.exclude_hv = 1;
 
 	// for each CPU:
-	// create a systemwide monitor restricted to a set of CPUs
+	// create a systemwide monitor restricted to a set of CPUs to monitor cache misses
+	for (size_t i = 0; i < distgen_init.number_of_threads; ++i) {
+		auto fd = perf_event_open(&event, -1, i, -1, 0);
+		if (fd == -1) {
+			std::cerr << "Could not create perf event for core " << i
+					  << ". Please set /proc/sys/kernel/perf_event_paranoid to -1." << std::endl;
+			return std::vector<int>();
+		}
+		fds.push_back(fd);
+	}
+
+	event.config = PERF_COUNT_HW_INSTRUCTIONS;
 	for (size_t i = 0; i < distgen_init.number_of_threads; ++i) {
 		auto fd = perf_event_open(&event, -1, i, -1, 0);
 		if (fd == -1) {
@@ -178,18 +199,6 @@ static std::vector<long long> read_perf_measurement(const std::vector<int> &fds)
 	return counts;
 }
 
-// run distgen to evict everything from the cache
-static void clear_cache() {
-	cgroup_freeze(res_name);
-
-	distgend_configT dg;
-	dg.number_of_threads = distgen_init.number_of_threads;
-	for (unsigned char i = 0; i < distgen_init.number_of_threads; ++i) dg.threads_to_use[i] = i;
-	distgend_is_membound(dg);
-
-	cgroup_thaw(res_name);
-}
-
 static void execute_command_internal(std::string command) {
 	cgroup_add_me(res_name);
 	// command += "| tee ";
@@ -203,66 +212,105 @@ static void execute_command_internal(std::string command) {
 
 static void execute_command(std::string command) { thread_pool.emplace_back(&execute_command_internal, command); }
 
+static std::bitset<64> create_minimal_bitset() {
+	std::bitset<64> bits;
+	for (size_t i = 0; i < get_min_cbm_bits(); ++i) {
+		bits.set(i);
+	}
+	return bits;
+}
+
+static std::bitset<64> increase_bitset(std::bitset<64> bits) {
+	for (size_t i = 0; i < bits.size(); ++i) {
+		if (!bits[i]) {
+			bits[i] = true;
+			break;
+		}
+	}
+	return bits;
+}
+
+// run distgen to evict everything from the cache
+static void clear_cache() {
+	cgroup_freeze(res_name);
+
+	distgend_configT dg;
+	dg.number_of_threads = distgen_init.number_of_threads;
+	for (unsigned char i = 0; i < distgen_init.number_of_threads; ++i) dg.threads_to_use[i] = i;
+	distgend_is_membound(dg);
+
+	cgroup_thaw(res_name);
+}
+
 int main(int argc, char const *argv[]) {
 	parse_options(static_cast<size_t>(argc), argv);
-
-	std::cout << "Analysing " << command << std::endl << std::endl;
-
-	std::cout << "Starting distgen initialization ...";
-	std::cout.flush();
-	distgend_init(distgen_init);
-	std::cout << " done!\n\n";
 
 	std::cout << "CBM max: " << std::hex << get_cbm_mask_as_uint() << std::endl;
 	std::cout << "CBM min bits: " << get_min_cbm_bits() << std::endl;
 	std::cout << "Number of closids: " << get_num_closids() << std::endl;
 	std::cout << std::endl;
 
+	if (use_cache_clear) {
+		std::cout << "Starting distgen initialization ...";
+		std::cout.flush();
+		distgend_init(distgen_init);
+		std::cout << " done!\n\n";
+	}
+
 	auto perf_ids = init_perf();
 
 	setup_cat();
 
-	std::cout << "Mask"
-			  << " - "
-			  << "cache misses" << std::endl;
+	std::cout << "Analysing " << command << " " << std::flush;
 
-	// loop over all possible settings
-	auto bits = get_cbm_mask();
+	auto bits = create_minimal_bitset();
+	std::vector<double> llc_misses;
+	std::vector<double> instruction_count;
 
 	execute_command(command);
 	// wait some time before we start measurements
-	std::this_thread::sleep_for(measurement_time);
+	std::this_thread::sleep_for(measurement_time * 3);
 
-	while (bits.count() >= get_min_cbm_bits()) {
-
+	// loop over all possible settings
+	while (true) {
 		std::vector<size_t> schematas;
 		for (size_t n = 0; n < distgen_init.NUMA_domains; ++n) schematas.push_back(bits.to_ullong());
 		resgroup_set_schemata(res_name, schematas);
 
-		clear_cache();
+		if (use_cache_clear) {
+			clear_cache();
+		}
 
 		start_perf_measurement(perf_ids);
 		std::this_thread::sleep_for(measurement_time);
 		stop_perf_measurement(perf_ids);
 
 		auto res = read_perf_measurement(perf_ids);
-		double sum = 0;
-		for (size_t i = 0; i < res.size(); ++i) {
-			sum += res[i];
-		}
 
-		std::cout << std::hex << bits.to_ullong() << " - " << std::dec << sum << std::endl;
+		llc_misses.push_back(std::accumulate(res.begin(), res.begin() + distgen_init.number_of_threads, 0.0));
+		instruction_count.push_back(std::accumulate(res.begin() + distgen_init.number_of_threads, res.end(), 0.0));
 
-		// reset one bit
-		for (size_t i = 0; i < bits.size(); ++i) {
-			if (bits[i]) {
-				bits[i] = false;
-				break;
-			}
-		}
+		std::cout << "." << std::flush;
+
+		if (bits == get_cbm_mask()) break;
+		bits = increase_bitset(bits);
 	}
 
-	std::cout << "\nMeasurement done!" << std::endl;
+	std::cout << " measurement done!" << std::endl << std::endl;
+
+	bits = create_minimal_bitset();
+
+	const double max_cache = llc_misses[llc_misses.size() - 1];
+	const double max_instruction = instruction_count[instruction_count.size() - 1];
+
+	std::cout << "mask \t\t llc \t\t llc(nom) \t\t instr \t\t instr(nom)" << std::endl;
+	for (size_t i = 0; i < llc_misses.size(); ++i) {
+		std::cout << std::hex << bits.to_ullong() << " \t\t " << std::dec << llc_misses[i] << " \t "
+				  << llc_misses[i] / max_cache << " \t " << instruction_count[i] << " \t "
+				  << instruction_count[i] / max_instruction << std::endl;
+
+		bits = increase_bitset(bits);
+	}
 
 	cleanup();
 }
